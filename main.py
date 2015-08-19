a98fdf1b6be680b2faf0c427b36b70b60d7bbc3e
a_leontiev <andruwwwka@gmail.com>
2015-08-20 00:58:37 +0300

    Вроде поразобрался с урлами

--- main.py
+++ main.py
@@ -23,14 +23,21 @@ class SimpleSpider(Spider):
 
     def task_parse(self, grab, task):
         #defaul 'title' or h1
+        # Заголовок
         for elem in grab.doc.select('//h1[@class="b-topic__title"]'):
             print(elem._node.text_content())
         #default '//div/p' для получения ссылки '//div[@class="b-text clearfix"]//p/a/@href'
         # '//div[@class="b-text clearfix"]//p/a[@class="source"]/@href'
+        # Текст
         for elem in grab.doc.select('//div[@class="b-text clearfix"]//p'):
             print(elem._node.text_content())
+            # Можно внутри прямо переберать текущий элемент с использованием elem.select('//div[@class="b-text clearfix"]//p/a')
+        # Ссылка
         for elem in grab.doc.select('//div[@class="b-text clearfix"]//p/a[@class="source"]/@href'):
             print(elem._node)
+        # Текст в ссылке
+        for elem in grab.doc.select('//div[@class="b-text clearfix"]//p/a[@class="source"]'):
+            print(elem._node.text_content())
 
 
 if __name__ == '__main__':
